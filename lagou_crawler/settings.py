eb95687e619397a33b99a6735c71527962911f83
RyanZhu <troyzx1024@gmail.com>
2015-04-23 23:33:22 +0800

    init scrapy

---
+++ lagou_crawler/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for lagou_crawler project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'lagou_crawler'
+
+SPIDER_MODULES = ['lagou_crawler.spiders']
+NEWSPIDER_MODULE = 'lagou_crawler.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'lagou_crawler (+http://www.yourdomain.com)'
