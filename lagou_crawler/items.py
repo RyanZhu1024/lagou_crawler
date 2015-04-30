11d07b455e937c029f3f7be987f80be288981d7e
RyanZhu <troyzx1024@gmail.com>
2015-04-30 15:48:58 +0800

    add spider

--- lagou_crawler/items.py
+++ lagou_crawler/items.py
@@ -5,10 +5,11 @@
 # See documentation in:
 # http://doc.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy.item import Item, Field
 
 
-class LagouCrawlerItem(scrapy.Item):
+class LagouCrawlerItem(Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    title = Field()
+    link = Field()
+    desc = Field()
---
+++ lagou_crawler/spiders/LagouSpider.py
@@ -0,0 +1,14 @@
+from scrapy import Spider
+
+__author__ = 'shuxuan'
+
+
+class LagouSpider(Spider):
+    name = "lagou"
+    allowed_domains = ["dmoz.org"]
+    start_urls = [
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
+    ]
+
+    def parse(self, response):
